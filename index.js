8b6ac24a2af12f21cae7a3f6c55d7842ed3af679
Jonathan Mines <minesja@gmail.com>
2017-09-15 22:21:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ function happyHolidays() {
   return "Happy holidays!"
 }
 
-function happyHolidaysTo(name) {
+function happyHolidaysTo(${name}) {
   return "Happy holidays, ${name}!";
 }
 
 function happyHolidayTo(holiday, name) {
-  return 'Happy ${holiday}, ${name}!';
+  return "Happy ${holiday}, ${name}!";
 }
 
 function holidayCountdown(days, holiday) {
